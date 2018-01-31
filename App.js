30d317238790c62dcbf11c57727f4a78b565d400
Richard Zhang <hmzhang@uwaterloo.ca>
2018-01-30 21:52:28 -0500

    added popup and date picker

--- App.js
+++ App.js
@@ -5,6 +5,9 @@
  */
 
 import React, { Component } from 'react';
+import PopupDialog from 'react-native-popup-dialog';
+import DatePicker from 'react-native-datepicker'
+import FAB from 'react-native-fab';
 import {
   Platform,
   StyleSheet,
@@ -26,6 +29,34 @@ export default class App extends Component<{}> {
           renderItem = {({item}) => <Text style = {styles.mainListViewRow}> {item} </Text> }
           renderSectionHeader = {({section}) => <Text style = {styles.mainListViewHeader}> {section.title} </Text>}
           keyExtractor = {(item, index) => index} />
+          <FAB buttonColor="blue" visible={true} onClickAction={()=>{this.addNewGoalPopup.show();}}/>
+          <PopupDialog ref = {(addNewGoalPopup) => { this.addNewGoalPopup = addNewGoalPopup;}}>
+            <View>
+              <Text style = {styles.popupText}> Add a new goal. </Text>
+              <DatePicker
+               style={{width: 300}}
+               date={"2018-01-30"}
+               mode="date"
+               format="YYYY-MM-DD"
+               minDate="2018-01-30"
+               maxDate="2019-12-31"
+               confirmBtnText="Confirm"
+               cancelBtnText="Cancel"
+               customStyles={{
+                 dateIcon: {
+                   position: 'absolute',
+                   top: 16,
+                   left: 40,
+                 },
+                 dateInput: {
+                   top: 16,
+                   marginLeft: 100,
+                 }
+               }}
+               onDateChange={(date) => {this.setState({date: date})}}
+               />
+            </View>
+          </PopupDialog>
       </View>
     );
   }
@@ -45,6 +76,13 @@ const styles = StyleSheet.create({
   },
   mainListViewHeader: {
     fontSize: 40,
+    textAlign: 'left',
     padding: 25,
+  },
+  popupText: {
+    fontSize: 24,
+    marginTop: 24,
+    textAlign: 'center',
+
   },
 });
