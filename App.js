45de29a2a7de1c068038f06a7ca457444df3c3d6
Richard Zhang <hmzhang@uwaterloo.ca>
2018-01-31 20:50:59 -0500

    finished initial UI for add new goal popup

--- App.js
+++ App.js
@@ -13,7 +13,8 @@ import {
   StyleSheet,
   SectionList,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
 
 export default class App extends Component<{}> {
@@ -32,7 +33,8 @@ export default class App extends Component<{}> {
           <FAB buttonColor="blue" visible={true} onClickAction={()=>{this.addNewGoalPopup.show();}}/>
           <PopupDialog ref = {(addNewGoalPopup) => { this.addNewGoalPopup = addNewGoalPopup;}}>
             <View>
-              <Text style = {styles.popupText}> Add a new goal. </Text>
+              <Text style = {styles.popupTextHeader}> Add a new goal. </Text>
+              <Text style = {styles.popupText}> Choose your start date. </Text>
               <DatePicker
                style={{width: 300}}
                date={"2018-01-30"}
@@ -56,6 +58,36 @@ export default class App extends Component<{}> {
                onDateChange={(date) => {this.setState({date: date})}}
                />
             </View>
+            <Text style = {styles.popupText}> Choose your end date. </Text>
+            <DatePicker
+             style={{width: 300}}
+             date={"2018-01-30"}
+             mode="date"
+             format="YYYY-MM-DD"
+             minDate="2018-01-30"
+             maxDate="2019-12-31"
+             confirmBtnText="Confirm"
+             cancelBtnText="Cancel"
+             customStyles={{
+               dateIcon: {
+                 position: 'absolute',
+                 top: 16,
+                 left: 40,
+               },
+               dateInput: {
+                 top: 16,
+                 marginLeft: 100,
+               }
+             }}
+             onDateChange={(date) => {this.setState({date: date})}}
+             />
+             <View style = {styles.popupButtonView} >
+             <Button
+             onPress={()=>{this.addNewGoalPopup.dismiss();}}
+             title="Submit"
+             color="#42a1f4"
+             />
+             </View>
           </PopupDialog>
       </View>
     );
@@ -79,10 +111,18 @@ const styles = StyleSheet.create({
     textAlign: 'left',
     padding: 25,
   },
-  popupText: {
+  popupTextHeader: {
     fontSize: 24,
     marginTop: 24,
     textAlign: 'center',
-
+  },
+  popupText: {
+    fontSize: 16,
+    marginTop: 16,
+    textAlign: 'center',
+  },
+  popupButtonView: {
+    justifyContent: 'center',
+    marginTop: 24,
   },
 });
